f74dfddad9dec5f45204cec1c3368856a10d83bf
nazmu957 <nazmus15-1999@diu.edu.bd>
2024-01-01 22:40:15 +0600

    search bar added

---
+++ src/components/UI/Topbar.js
@@ -0,0 +1,24 @@
+
+
+const Topbar = () => {
+    return (
+      <div>
+        <nav class="bg-white text-black p-4">
+          <div class="container mx-auto flex justify-between items-center">
+            <a href="#" class=" text-lg font-bold">
+              JobSeekers
+            </a>
+
+            <div class="space-x-4">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Jobs</a>
+              <a href="#">Others</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+};
+
+export default Topbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,48 +2,30 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 
+import Topbar from "@/components/UI/Topbar";
+import Searchbar from "@/components/UI/Searchbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
-const HomePage = ({  }) => {
-
-    return (
-      <>
-        <Head>
-          <title>Travel-Gide</title>
-          <meta
-            name="description"
-            content="This is travel web site made by next-js"
-          />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="https://i.ibb.co/Scywkr8/2461656.jpg" />
-          <div>
-            <div>
-              <nav class="bg-gray-800 p-4">
-                <div class="container mx-auto flex justify-between items-center">
-                  <a href="#" class="text-white text-lg font-bold">
-                    JobSeekers
-                  </a>
-
-                  <div class="space-x-4">
-                    <a href="#" class="text-white">
-                      Home
-                    </a>
-                    <a href="#" class="text-white">
-                      About
-                    </a>
-                    <a href="#" class="text-white">
-                      Jobs
-                    </a>
-                    <a href="#" class="text-white">
-                      Others
-                    </a>
-                  </div>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </Head>
-      </>
-    );
+const HomePage = ({}) => {
+  return (
+    <>
+      <Head>
+        <title>JobSeekers</title>
+        <meta
+          name="description"
+          content="This is travel web site made by next-js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://i.ibb.co/Scywkr8/2461656.jpg" />
+      
+      </Head>
+      <Topbar  />
+      <Searchbar />
+    </>
+  );
 };
 export default HomePage;
+
+
+
