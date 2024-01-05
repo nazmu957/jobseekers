68a2cc05a9071733424d7e0397fcc7ad1dc6c69c
nazmu957 <nazmus15-1999@diu.edu.bd>
2024-01-05 15:57:47 +0600

    solved the login issue

--- src/components/UI/Topbar.js
+++ src/components/UI/Topbar.js
@@ -1,45 +1,36 @@
-
 import { AuthContext } from "@/contexts/UserContext";
 import { Button } from "antd";
 import Link from "next/link";
 import { useContext } from "react";
 
-
 const Topbar = () => {
   const { user, logOut } = useContext(AuthContext);
-    return (
-      <div className="border">
-        <nav class="bg-white text-black p-4">
-          <div class="container mx-auto flex justify-between items-center">
-            <a href="#" class=" text-lg font-bold">
-              JobSeekers
-            </a>
+  return (
+    <div className="border">
+      <nav class="bg-white text-black p-4">
+        <div class="container mx-auto flex justify-between items-center">
+          <a href="#" class=" text-lg font-bold">
+            JobSeekers
+          </a>
 
-            <div class="space-x-4">
-              <Link href="/">Home</Link>
-              <Link href="/profile">Profile</Link>
-              <Link href="/">Jobs</Link>
-              <Link href="/">Others</Link>
-              <Link href="/dashboard">Dashboard</Link>
+          <div class="space-x-4">
+            <Link href="/">Home</Link>
+            <Link href="/profile">Profile</Link>
+            <Link href="/">Jobs</Link>
+            <Link href="/">Others</Link>
+            <Link href="/dashboard">Dashboard</Link>
+            <>
               {user?.uid ? (
-                <items>
-                  <Button onClick={logOut} type="primary" danger>
-                    Logout
-                  </Button>
-                </items>
+                <button onClick={logOut}>logout</button>
               ) : (
-                <Link
-                  style={{ textDecoration: "none", color: "white" }}
-                  href="/LoginForm"
-                >
-                  <p className="text-black">Login</p>
-                </Link>
+                <Link href="/LoginForm">Login</Link>
               )}
-            </div>
+            </>
           </div>
-        </nav>
-      </div>
-    );
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Topbar;
