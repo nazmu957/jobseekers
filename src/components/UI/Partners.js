b2fe842607ae1d4af61da315668fcaa620391c68
nazmu957 <nazmus15-1999@diu.edu.bd>
2024-05-22 00:14:08 +0600

    partnar card animation add

--- src/components/UI/Partners.js
+++ src/components/UI/Partners.js
@@ -2,48 +2,129 @@ import React from "react";
 
 
 
+
+
 const Partners = () => {
+
+
+
   return (
-    <div className="h-auto">
-      <div className="flex flex-wrap justify-center mx-4 md:mx-8 lg:mx-16 xl:mx-20 2xl:mx-32 py-4">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+    // <div className="h-auto">
+    //   <div className="flex flex-wrap justify-center mx-4 md:mx-8 lg:mx-16 xl:mx-20 2xl:mx-32 py-4">
+    //     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+    //       <img
+    //         src="https://static.vecteezy.com/system/resources/previews/027/127/592/non_2x/microsoft-logo-microsoft-icon-transparent-free-png.png"
+    //         alt=""
+    //         className="w-full lg:w-[7rem] "
+    //       />
+    //     </div>
+    //     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+    //       <img
+    //         src="https://motorsbrand.com/wp-content/uploads/2023/05/tata-logo.png"
+    //         alt=""
+    //         className="w-full lg:w-[7rem] lg:pt-5"
+    //       />
+    //     </div>
+    //     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+    //       <img
+    //         src="https://w7.pngwing.com/pngs/523/198/png-transparent-google-logo-google-search-google-play-google-text-logo-number.png"
+    //         alt=""
+    //         className="w-full lg:w-[7rem] lg:pt-5"
+    //       />
+    //     </div>
+    //     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+    //       <img
+    //         src="https://logowik.com/content/uploads/images/tesla.jpg"
+    //         alt=""
+    //         className="w-full lg:w-[7rem] lg:pt-5"
+    //       />
+    //     </div>
+    //     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+    //       <img
+    //         src="https://cdn.freelogovectors.net/wp-content/uploads/2023/03/amazon-logo-freelogovectors.net_.png"
+    //         alt=""
+    //         className="w-full lg:w-[7rem] lg:pt-5"
+    //       />
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="h-auto overflow-hidden">
+      <div className="flex flex-nowrap animate-marquee whitespace-nowrap">
+        {/* Original set of cards */}
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
           <img
             src="https://static.vecteezy.com/system/resources/previews/027/127/592/non_2x/microsoft-logo-microsoft-icon-transparent-free-png.png"
-            alt=""
-            className="w-full lg:w-[7rem] "
+            alt="Microsoft Logo"
+            className="w-full lg:w-[7rem]"
           />
         </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
           <img
             src="https://motorsbrand.com/wp-content/uploads/2023/05/tata-logo.png"
-            alt=""
+            alt="Tata Logo"
             className="w-full lg:w-[7rem] lg:pt-5"
           />
         </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
           <img
             src="https://w7.pngwing.com/pngs/523/198/png-transparent-google-logo-google-search-google-play-google-text-logo-number.png"
-            alt=""
+            alt="Google Logo"
             className="w-full lg:w-[7rem] lg:pt-5"
           />
         </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
           <img
             src="https://logowik.com/content/uploads/images/tesla.jpg"
-            alt=""
+            alt="Tesla Logo"
             className="w-full lg:w-[7rem] lg:pt-5"
           />
         </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
           <img
             src="https://cdn.freelogovectors.net/wp-content/uploads/2023/03/amazon-logo-freelogovectors.net_.png"
-            alt=""
+            alt="Amazon Logo"
+            className="w-full lg:w-[7rem] lg:pt-5"
+          />
+        </div>
+        {/* Duplicate set of cards for seamless animation */}
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/027/127/592/non_2x/microsoft-logo-microsoft-icon-transparent-free-png.png"
+            alt="Microsoft Logo"
+            className="w-full lg:w-[7rem]"
+          />
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
+          <img
+            src="https://motorsbrand.com/wp-content/uploads/2023/05/tata-logo.png"
+            alt="Tata Logo"
+            className="w-full lg:w-[7rem] lg:pt-5"
+          />
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
+          <img
+            src="https://w7.pngwing.com/pngs/523/198/png-transparent-google-logo-google-search-google-play-google-text-logo-number.png"
+            alt="Google Logo"
+            className="w-full lg:w-[7rem] lg:pt-5"
+          />
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
+          <img
+            src="https://logowik.com/content/uploads/images/tesla.jpg"
+            alt="Tesla Logo"
+            className="w-full lg:w-[7rem] lg:pt-5"
+          />
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-8 inline-block px-4">
+          <img
+            src="https://cdn.freelogovectors.net/wp-content/uploads/2023/03/amazon-logo-freelogovectors.net_.png"
+            alt="Amazon Logo"
             className="w-full lg:w-[7rem] lg:pt-5"
           />
         </div>
       </div>
     </div>
-    
   );
 };
 
